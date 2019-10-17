f0f7c3db248458855f0056e111170362c6681068
spiritualized <jeff.warren@gmail.com>
2019-10-17 00:05:18 -0400

    Move release categories to constants.py

---
+++ metafix/constants.py
@@ -0,0 +1,3 @@
+valid_release_categories = ['Album', 'Soundtrack', 'EP', 'Anthology', 'Compilation', 'Single', 'Live Album',
+                                    'Remix', 'Bootleg', 'Interview', 'Mixtape', 'Demo', 'Concert Recording', 'Mix',
+                                    'Unknown']
