3fe11385c81c0c3be44c8313579918ba4b1c079f
spiritualized <contact@spiritualized.io>
2019-10-17 23:58:47 -0400

    Handle lastfmcache artist not found, miscellaneous fixes

--- metafix/constants.py
+++ metafix/constants.py
@@ -1,3 +1,7 @@
-valid_release_categories = ['Album', 'Soundtrack', 'EP', 'Anthology', 'Compilation', 'Single', 'Live Album',
+valid_release_categories = {'Album', 'Soundtrack', 'EP', 'Anthology', 'Compilation', 'Single', 'Live Album',
                                     'Remix', 'Bootleg', 'Interview', 'Mixtape', 'Demo', 'Concert Recording', 'Mix',
-                                    'Unknown']
+                                    'Unknown'}
+
+audio_extensions = {'.mp3', '.flac', '.aac', '.mp4', '.m4a', '.m4b', '.m4p', '.mmf', '.mpc' '.wav', '.ape', '.wv',
+                    '.aiff', '.au', '.pcm', '.wma', '.aa', '.aax', '.alac', '.amr', '.au', '.awb', '.dct', '.dss',
+                    '.dvf', '.gsm', '.iklax', '.ivs', '.ogg', '.oga', '.mogg', '.ra', '.sln', '.tta', '.8svx'}
